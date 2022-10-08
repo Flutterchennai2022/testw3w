6b3dbdfa4b6252574d9557174e371e27436834aa
Flutterchennai2022 <testgdriveindia@gmail.com>
2022-10-08 08:31:30 +0530

    first commit4

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/material.dart';
-import 'package:animated_splash_screen/animated_splash_screen.dart';
-void main() {
-  runApp(const MyApp());
-}
+import 'package:webview_flutter/webview_flutter.dart';
+
+void main() => runApp(MyApp());
+
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+  late WebViewController _controller;
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -12,39 +13,18 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: AnimatedSplashScreen(
-        splash: 'assets/facebook.png', // use any widget here
-        nextScreen: MyHomePage(),
-        splashTransition: SplashTransition.rotationTransition,
-        duration: 3000,
-      )
-    );
-  }
-}
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key}) : super(key: key);
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-class _MyHomePageState extends State<MyHomePage> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            // ignore: sized_box_for_whitespace
-            Container(
-              width: 150,
-              height: 150,
-              child: Image.asset('assets/logo.png'),
-            ),
-            // ignore: avoid_unnecessary_containers
-            Container(
-              child: const Text("Welcome to Proto Coders Point",style: TextStyle(fontSize: 20),),
-            ),
-          ],
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text("TutorialKart - Flutter WebView"),
+        ),
+        body: Center(
+          child: WebView(
+            initialUrl: 'https://www.tutorialkart.com/',
+            javascriptMode: JavascriptMode.unrestricted,
+            onWebViewCreated: (WebViewController webViewController) {
+              _controller = webViewController;
+            },
+          ),
         ),
       ),
     );
